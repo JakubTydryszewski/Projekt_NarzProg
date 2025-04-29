52ba46a0429850b1b93bbd690872699830c9d4e1
JakubTydryszewski <104149274+JakubTydryszewski@users.noreply.github.com>
2025-04-29 20:02:52 +0200

    Add files via upload

--- src/connect.php
+++ src/connect.php
@@ -1,6 +1,13 @@
 <?php
-    $host = "db"; // Use the service name defined in docker-compose.yml
-    $db_user = "root"; // Default MySQL user
-    $db_password = "root_password"; // Set your MySQL root password
-    $db_name = "serwer_arch"; // Database name
+    $host = "db"; // MySQL service name in docker-compose.yml
+    $db_user = "root";
+    $db_password = "root_password";
+    $db_name = "serwer_arch";
+
+    $conn = mysqli_connect($host, $db_user, $db_password, $db_name);
+
+    if (!$conn) {
+        die("Connection failed: " . mysqli_connect_error());
+    }
+    echo "Connected successfully";
 ?>
